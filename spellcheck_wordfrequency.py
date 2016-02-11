131e978a13de47c307a055fbca32710386149ab5
Hannes Roest <roest@imsb.biol.ethz.ch>
2016-02-10 19:07:44 -0800

    adjust (blind)

--- spellcheck_wordfrequency.py
+++ spellcheck_wordfrequency.py
@@ -1,30 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+Script that identifies misspelled words from a SQL table that contains two columns:
+
+    - smallword : the word itself
+    - occurence : the word frequency
+
+""" 
+import MySQLdb
 
 import pagegenerators
-import MySQLdb
 import wikipedia as pywikibot
-import BlacklistChecker as temp
-from spellcheck_wordlist import BlacklistSpellchecker
-import BlacklistChecker
+
+from wikispell.BlacklistSpellchecker import BlacklistSpellchecker
+from wikispell.BlacklistChecker import BlacklistChecker
+
 ################################################################################
-# db = MySQLdb.connect(read_default_file="~/.my.cnf.hroest", charset = "utf8", use_unicode = True)
-db = MySQLdb.connect(read_default_file="~/.my.cnf.hroest")
-cursor = db.cursor()
 
-sp = BlacklistSpellchecker()
-# db_dump = "20151002_n";
-db_dump = "20160111";
+# Database config
+MYSQL_CONFIG = "~/.my.cnf.hroest"
 db_dump = "20160203";
-
-# Top 10k words:
-# +-----------+---------------------------+
-# |      3190 | Kunde                     |
-# +-----------+---------------------------+
-# 10000 rows in set (0.02 sec)
 
 ################################################################################
 # Find the most common words and search for missspells of those 
-# {{{
 ################################################################################
+db = MySQLdb.connect(read_default_file=MYSQL_CONFIG)
+cursor = db.cursor()
 
 cursor.execute(
 """
@@ -34,26 +35,32 @@ order by occurence DESC
 """  % db_dump)
 misspell = cursor.fetchall()
 
-import BlacklistChecker
 bb = BlacklistChecker.Blacklistchecker()
+sp = BlacklistSpellchecker()
 
 try:
     j = -1
-    #for j in range(700):
     while j < len(misspell):
         j += 1
-        if j < 1000: continue
-        # if j < 2717: continue
         myw = misspell[j][1]
         myw = myw.decode('utf8')
         print "================================="
         print myw, j, "out of", len(misspell)
+        ## TODO: make sure you adjust these parameters:
+        #   - occurence_cutoff
+        #   - lcutoff
+        #   - ldistance
+        #   - askUser
         candidates = bb.find_candidates(myw, cursor,
                                 occurence_cutoff = 25, lcutoff = 0.85, ldistance = 2,
                                 db_='wikiwords.countedwords_%s' % db_dump)
         print "nr candidates", len(candidates)
-        if len(candidates) == 0: continue
+
+        if len(candidates) == 0: 
+            continue
+
         pages = bb.load_candidates(myw, candidates, askUser=False) 
+
         print "will do", len(pages), "pages"
         wrongwordlist = [p.wrong for p in pages]
         gen = pagegenerators.PreloadingGenerator(pages)
@@ -69,4 +76,3 @@ except Exception as e:
 
 bb.store_wikipedia()
 
-# }}}
